d0acdcd6d97085cad46664d6d56c5c72645eba41
Kirill <kirill.2201@inbox.ru>
2022-04-26 22:54:28 +0700

    complete gallery

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let offset = 0;
 const sliderLine = document.querySelectorAll(".slider__line");
 
 document.querySelector(".btn-next").addEventListener("click", () => {
+    offset === 0? offset = -1600:offset += 400;
+    sliderLine.forEach((item) => item.style.left = offset + "px");
+})
+
+document.querySelector(".slider__item-last").addEventListener("click", () => {
     offset < -1200? offset = 0:offset -= 400;
     sliderLine.forEach((item) => item.style.left = offset + "px");
 })
