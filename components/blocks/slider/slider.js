ae5ae353caa4a153575e27c0019e5f84bb62ebe9
Kirill <kirill.2201@inbox.ru>
2022-04-27 15:57:50 +0700

    refactor file sctructure

--- components/blocks/slider/slider.js
+++ components/blocks/slider/slider.js
@@ -1,12 +1,12 @@
 let offset = 0;
 const sliderLine = document.querySelectorAll(".slider__line");
 
-document.querySelector(".btn-next").addEventListener("click", () => {
+document.querySelector(".btn-back").addEventListener("click", () => {
     offset === 0? offset = -1600:offset += 400;
     sliderLine.forEach((item) => item.style.left = offset + "px");
 })
 
-document.querySelector(".slider__item-last").addEventListener("click", () => {
+document.querySelector(".slider__item--size--slim").addEventListener("click", () => {
     offset < -1200? offset = 0:offset -= 400;
     sliderLine.forEach((item) => item.style.left = offset + "px");
 })
